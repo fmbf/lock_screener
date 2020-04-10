1c073c943df14247cfc5085bf09508059156d8a3
fmbf <circumnavigate.the.globe@gmail.com>
2020-04-10 14:01:08 -0700

    styling cleanup and readme updates

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# lock_screener
+# Lock Screener
+
+Random Wallpaper Generator made with React
+
+<p align="center">
+  <img src="lock-screener.gif">
+</p>
+
+Resources:
+- https://unsplash.com/@hdwallpaper
+- https://source.unsplash.com/
--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
 let initBackground = 'https://images.unsplash.com/photo-1535200011543-21d1d3247768?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2734&q=80';
 
-const phrases = [
-  'looking freshh...',
-  'your new lock screen...',
-  'try this one...',
-  'hmm...',
-];
 
 const initState = {
   isLoading: false,
   currentBackground: initBackground,
   backgrounds: [initBackground],
-  phraseIdx: 0,
 };
-
-
-
 
 class App extends Component {
   constructor(props) {
@@ -36,14 +25,11 @@ class App extends Component {
       currentBackground: url,
       backgrounds: this.state.backgrounds.concat([url]),
       isLoading: false,
-      phraseIdx: this.state.phraseIdx + 1,
     });
-
-    console.log('state:', this.state);
   }
 
   fetchRandomPic = () => {
-    const { currentBackground } = this.state;
+    const { currentBackground, isLoading } = this.state;
 
     this.isLoading(true);
 
@@ -51,8 +37,6 @@ class App extends Component {
 
     req.onreadystatechange = () => {
       if (req.readyState === 4 && req.status === 200) {
-        // console.log('responded with:', req.responseURL);
-
         currentBackground !== req.responseURL
           ? this.addNewPic(req.responseURL)
           : this.fetchRandomPic();
@@ -68,79 +52,37 @@ class App extends Component {
       isLoading,
       currentBackground,
       backgrounds,
-      phraseIdx
     } = this.state;
 
     return (
       <div className="App">
+        <div className="slanted-bg" />
+        <div className="corner-overlay" />
+
+        <a className="social-icon" href='https://github.com/fmbf' target="_blank">
+          <ion-icon name="logo-github" />
+        </a>
 
         <div className="main-container">
           <div className="phone-box">
             <div className={"bkg-pic-container" + (isLoading ? ' loading' : '')}>
               <img alt="content" className="bkg-pic" src={isLoading ? backgrounds[backgrounds.length - 1] : currentBackground} />
             </div>
-
-
-            <img alt="content" className="phone" src={require("../assets/phone_overlay_light 4.png")} />
+            <img alt="content" className="phone" src={require("../assets/phone_overlay_light.png")} />
+            <img alt="loading-indicator" src={require('../assets/loading_indicator.svg')} style={{ display: isLoading ? 'block' : '' }} />
           </div>
 
-
-
-
-
           <div className="sidebar">
-            <a href='https://github.com/fmbf' target="_blank">
-              <p style={{ fontSize: 48, marginBottom: '0.51rem', fontFamily: 'sans-serif', color: '#f4f4f4' }}>
-                lock-screener
-              </p>
-            </a>
-
-            <p className="header">
-              {phrases[phraseIdx % phrases.length]}
-            </p>
-
-
             <div className="controls-container">
-
-
-
-
-              <div className="button" onClick={this.fetchRandomPic}>
+              <div className="button" onClick={() => !isLoading ? this.fetchRandomPic() : null}>
                 <ion-icon name="shuffle"></ion-icon>
               </div>
-
               <a className="button" href={currentBackground} target="_blank">
                 <ion-icon name="download"></ion-icon>
               </a>
-
-
-              {/*<div className="icon-box">
-
-                <span className="icon-button">
-                  <a href={currentBackground} target="_blank" download >
-                    <ion-icon name="download"></ion-icon>
-                  </a>
-                </span>
-                <span className="icon-button">
-                  <a href={currentBackground} target="_blank" download >
-                    <ion-icon name="eye"></ion-icon>
-                  </a>
-                </span>
-              </div>*/}
-
-
-              {/*<p style={{ fontSize: 98, fontFamily: 'serif' }}>
-                {phrases[phraseIdx % phrases.length]}
-              </p>*/}
-
             </div>
-
-
           </div>
-
-
         </div>
-
 
       </div>
     );
